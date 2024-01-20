c78c3db3206e2314a161ccc449ddcbf72831e55d
sh-ahn-23 <shahn523@kaist.ac.kr>
2024-01-21 01:04:16 +0900

    feat: implement rotating and moving camera

--- src/App.jsx
+++ src/App.jsx
@@ -36,12 +36,10 @@ function AnimationCanvas() {
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-      console.log(event);
       keyMap.current[event.code] = true;
     };
 
     const handleKeyUp = (event) => {
-      console.log(event);
       keyMap.current[event.code] = false;
     };
 
@@ -56,32 +54,44 @@ function AnimationCanvas() {
 
   useFrame(({ camera }, delta) => {
     const movementSpeed = 15;
+    const rotationSpeed = 1;
 
     const moveForward = new THREE.Vector3(0, 0, -1);
-    const moveRight = new THREE.Vector3(1, 0, 0);
-    const moveLeft = new THREE.Vector3(-1, 0, 0);
 
-    const rotation = new THREE.Euler(0, camera.rotation.y, 0);
+    const rotation = new THREE.Euler(
+      camera.rotation.x,
+      camera.rotation.y,
+      camera.rotation.z,
+      "YXZ"
+    );
     moveForward.applyEuler(rotation);
-    moveRight.applyEuler(rotation);
-    moveLeft.applyEuler(rotation);
 
     if (keyMap.current["ArrowDown"]) {
-      camera.position.y -= movementSpeed * delta;
+      console.log(camera.rotation);
+      camera.rotation.x -= rotationSpeed * delta;
+      // camera.rotation.z -= rotationSpeed * delta;
+
+      // camera.position.y -= movementSpeed * delta;
     }
     if (keyMap.current["ArrowUp"]) {
-      camera.position.y += movementSpeed * delta;
+      camera.rotation.x += rotationSpeed * delta;
+      // camera.rotation.z += rotationSpeed * delta;
     }
     if (keyMap.current["ArrowRight"]) {
-      camera.position.x += moveRight.x * movementSpeed * delta;
-      camera.position.z += moveRight.z * movementSpeed * delta;
+      camera.rotation.y -= rotationSpeed * delta;
+
+      // camera.position.x += moveRight.x * movementSpeed * delta;
+      // camera.position.z += moveRight.z * movementSpeed * delta;
     }
     if (keyMap.current["ArrowLeft"]) {
-      camera.position.x += moveLeft.x * movementSpeed * delta;
-      camera.position.z += moveLeft.z * movementSpeed * delta;
+      camera.rotation.y += rotationSpeed * delta;
+
+      // camera.position.x += moveLeft.x * movementSpeed * delta;
+      // camera.position.z += moveLeft.z * movementSpeed * delta;
     }
     if (keyMap.current["Space"]) {
       camera.position.x += moveForward.x * movementSpeed * delta;
+      camera.position.y += moveForward.y * movementSpeed * delta;
       camera.position.z += moveForward.z * movementSpeed * delta;
     }
   });
@@ -97,7 +107,7 @@ function AnimationCanvas() {
 function App() {
   return (
     <div className="anim">
-      <Canvas camera={{ position: [100, 5, 100], fov: 75 }} tabIndex={0}>
+      <Canvas camera={{ position: [0, 0, 0], fov: 75 }} tabIndex={0}>
         <ambientLight intensity={0.3} />
         <directionalLight position={[1, 1, 1]} intensity={2} />
         <AnimationCanvas />
