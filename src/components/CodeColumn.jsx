cf413d645a7758193e24b84ea469b37f75e886de
sh-ahn-23 <shahn523@kaist.ac.kr>
2024-01-23 01:35:19 +0900

    feat: implement play sound on hover

--- src/components/CodeColumn.jsx
+++ src/components/CodeColumn.jsx
@@ -1,59 +1,21 @@
-import React, { useRef } from "react";
+import React from "react";
 import CodeSnippet from "./CodeSnippet.jsx";
 import { useLocation } from "react-router-dom";
-import { useFrame, useThree } from "@react-three/fiber";
-import * as THREE from "three";
 
 export default function CodeColumn() {
   const location = useLocation();
   const code = location.state ? location.state.code : null;
-  console.log(code);
+  // console.log(code);
   const codeLines = code ? code.split("\n") : [];
   const codeRes = codeLines.map((line) => ({
     content: line.trim(),
     length: line.trim().length,
   }));
 
-  // const { camera } = useThree();
-  // const codeSnippetRefs = useRef([]);
-
-  // // Calculate the bounding box that contains all the code snippets
-  // const boundingBox = new THREE.Box3();
-  // codeSnippetRefs.current.forEach((ref) => {
-  //   if (ref.current) {
-  //     const box = new THREE.Box3().setFromObject(ref.current);
-  //     boundingBox.expandByPoint(box.min);
-  //     boundingBox.expandByPoint(box.max);
-  //   }
-  // });
-
-  // // Calculate the center and size of the bounding box
-  // const center = new THREE.Vector3();
-  // boundingBox.getCenter(center);
-  // const size = new THREE.Vector3();
-  // boundingBox.getSize(size);
-
-  // // Set the camera position to be above the bounding box
-  // camera.position.set(center.x, center.y + size.y * 1.5, center.z);
-
-  // // Set the camera to look at the center of the bounding box
-  // // camera.lookAt(center);
-
-  // // Use the useFrame hook to update the camera position during animation
-  // useFrame(() => {
-  //   // Calculate the target position based on the bounding box
-  //   const targetPosition = new THREE.Vector3(
-  //     center.x,
-  //     center.y + size.y * 0.8,
-  //     center.z
-  //   );
-
-  //   // Move the camera smoothly towards the target position
-  //   camera.position.lerp(targetPosition, 0.05);
-
-  //   // Set the camera to look at the center of the bounding box
-  //   camera.lookAt(center);
-  // });
+  const maxCodeLength = codeRes.reduce(
+    (max, codeSnippet) => Math.max(max, codeSnippet.length),
+    0
+  );
 
   return (
     <group>
@@ -62,8 +24,8 @@ export default function CodeColumn() {
           key={index}
           code={codeSnippet.content}
           length={codeSnippet.length}
+          maxCodeLength={maxCodeLength}
           idx={index}
-          // ref={(ref) => (codeSnippetRefs.current[index] = ref)}
         />
       ))}
     </group>
--- src/pages/CodeFloat.jsx
+++ src/pages/CodeFloat.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import CodeColumn from "../components/CodeColumn";
+import { Note1, Note2, Note3 } from "../models/MusicalNote";
 
 export default function CodeFloat({ code }) {
   return (
@@ -11,6 +12,12 @@ export default function CodeFloat({ code }) {
         <pointLight position={[10, 10, 10]} />
         <CodeColumn code={code} />
         <Environment background preset="dawn" blur={0.8} />
+        <Note1 position={[50, 30, -80]} rotation={[0, 1, 0]} />
+        <Note1 position={[-100, -300, -800]} rotation={[0, 0, 1]} />
+        <Note2 position={[-30, -30, 0]} rotation={[0.1, 0.4, 0]} />
+        <Note2 position={[0, 300, 400]} rotation={[-0.1, 0, 0]} />
+        <Note3 position={[100, -50, 200]} rotation={[0, -1, 0]} />
+        <Note3 position={[200, 100, -600]} rotation={[0, 1, -1]} />
         <OrbitControls />
       </Canvas>
     </div>
