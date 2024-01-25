670f26b77c97330b2ab56d507ab055aa2161fd64
sh-ahn-23 <shahn523@kaist.ac.kr>
2024-01-25 19:29:02 +0900

    feat: implement free atmos scene

--- src/pages/Atmos.jsx
+++ src/pages/Atmos.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import { Canvas } from "react-three-fiber";
 import { Environment, ScrollControls } from "@react-three/drei";
 import { Experience } from "../components/Experience";
+import SideNavigation from "../components/SideNavigation";
+import styles from "./Atmos.module.css";
 
 export default function Atmos() {
   return (
-    <Canvas>
-      <Environment background preset="dawn" blur={0.8} />
-      <Experience />
-    </Canvas>
+    <>
+      <div className={styles.SideNavigationOverlay}>
+        <SideNavigation
+          before={"/codefloat"}
+          next={"/codeball"}
+          curMode="free"
+        />
+      </div>
+      <Canvas>
+        <Environment background preset="dawn" blur={0.8} />
+        <Experience />
+      </Canvas>
+    </>
   );
 }
